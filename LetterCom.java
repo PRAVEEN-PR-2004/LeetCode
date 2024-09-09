6d00ddabf4b10559a641b6329e79ad0eec46f2fa
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-09-09 09:56:06 +0530

    lettercombination

---
+++ LetterCom.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+public class LetterCom {
+    static HashMap<Integer, String> hs = new HashMap<>();
+    static List<String> al = new ArrayList<>();
+
+    public List<String> letterCombinations(String digits) {
+        al.clear();
+        hs.put(1, "");
+        hs.put(2, "abc");
+        hs.put(3, "def");
+        hs.put(4, "ghi");
+        hs.put(5, "jkl");
+        hs.put(6, "mno");
+        hs.put(7, "pqrs");
+        hs.put(8, "tuv");
+        hs.put(9, "wxyz");
+        if (digits.length() == 0) {
+            return al;
+        }
+        com("", digits, al);
+        return al;
+    }
+
+    public static void com(String ans, String no, List<String> al) {
+        if (no.length() == 0) {
+            al.add(ans);
+            return;
+        }
+        int digit = no.charAt(0) - '0';
+        String combinations = hs.get(digit);
+        for (int i = 0; i < combinations.length(); i++) {
+            char toAdd = combinations.charAt(i);
+            com(ans + toAdd, no.substring(1), al);
+        }
+    }
+
+    public static void main(String[] args) {
+        LetterCom sol = new LetterCom();
+        String digits = "23"; // You can modify this input to test other cases
+        List<String> result = sol.letterCombinations(digits);
+        System.out.println("Combination: " + result);
+    }
+}
--- SudokuSolver.java
+++ SudokuSolver.java
@@ -103,3 +103,107 @@ public class SudokuSolver {
     }
     
 }
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int[][] board = new int[9][9];
+        
+//         for (int i = 0; i < 9; i++) {
+//             for (int j = 0; j < 9; j++) {
+//                 board[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         long startTime = System.currentTimeMillis();
+//         int timeLimit = 2000; // 2 seconds
+
+//         boolean isSolved = solveBoard(board, startTime, timeLimit);
+//         if (isSolved && isBoardFilled(board)) {
+//             printBoard(board);
+//         } else {
+//             System.out.println("No solution");
+//         }
+//     }
+
+//     private static boolean solveBoard(int[][] board, long startTime, int timeLimit) {
+//         for (int i = 0; i < 9; i++) {
+//             for (int j = 0; j < 9; j++) {
+//                 if (board[i][j] == 0) {
+//                     for (int num = 1; num <= 9; num++) {
+//                         if (isValidBoard(board, num, i, j)) {
+//                             board[i][j] = num;
+//                             long elapsedTime = System.currentTimeMillis() - startTime;
+//                             if (elapsedTime > timeLimit || solveBoard(board, startTime, timeLimit)) {
+//                                 return true;
+//                             }
+//                             board[i][j] = 0; // backtrack
+//                         }
+//                     }
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true; // all cells filled
+//     }
+
+//     private static boolean isValidBoard(int[][] board, int num, int row, int col) {
+//         return isValidRow(board, num, row) &&
+//                isValidCol(board, num, col) &&
+//                isValidBox(board, num, row, col);
+//     }
+
+//     private static boolean isValidRow(int[][] board, int num, int row) {
+//         for (int i = 0; i < 9; i++) {
+//             if (board[row][i] == num) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     private static boolean isValidCol(int[][] board, int num, int col) {
+//         for (int i = 0; i < 9; i++) {
+//             if (board[i][col] == num) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     private static boolean isValidBox(int[][] board, int num, int row, int col) {
+//         int boxRowStart = row - row % 3;
+//         int boxColStart = col - col % 3;
+//         for (int i = boxRowStart; i < boxRowStart + 3; i++) {
+//             for (int j = boxColStart; j < boxColStart + 3; j++) {
+//                 if (board[i][j] == num) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+
+//     // Check if the board is completely filled
+//     private static boolean isBoardFilled(int[][] board) {
+//         for (int i = 0; i < 9; i++) {
+//             for (int j = 0; j < 9; j++) {
+//                 if (board[i][j] == 0) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+
+//     // Print the Sudoku board
+//     private static void printBoard(int[][] board) {
+//         for (int i = 0; i < 9; i++) {
+//             for (int j = 0; j < 9; j++) {
+//                 System.out.print(board[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
